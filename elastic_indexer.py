a297485205fcf1c4d6c30a3afd93fb440786a2ff
yizhar <yizhar.gilboa@planckresolution.com>
2017-02-18 17:40:53 +0200

    added elasticsearch indexer for all processed lyrics

---
+++ elastic_indexer.py
@@ -0,0 +1,19 @@
+import gzip
+import json
+import glob
+import elasticsearch
+from elasticsearch import helpers
+import os
+
+es = elasticsearch.Elasticsearch()
+DATA_BASEPATH = os.environ.get('DATA_BASEPATH')
+ES_INDEX = 'allsonglyrics'
+ES_TYPE = 'song'
+for f in glob.glob(os.path.join(DATA_BASEPATH, "*")):
+    with open(f, "rb") as f:
+        song_dict = json.loads(gzip.decompress(f.read()).decode())
+    actions = [dict(_index=ES_INDEX,
+                    _type=ES_TYPE,
+                    _id="%s:%s" % (x['artist']['unique_name'], x['href']),
+                    _body=x) for x in song_dict]
+    helpers.bulk(es, actions)
