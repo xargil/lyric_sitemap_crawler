d24edfdd6e10b865abc9da8c9bbc6e0597313139
Omri A <devomri@gmail.com>
2017-02-21 21:31:35 +0200

    Added hebrew artists

--- SpidersConfiguration/shironet_config.py
+++ SpidersConfiguration/shironet_config.py
@@ -3,7 +3,7 @@ from Model.artist_url import artist_url
 class shironet_config:
     SHIRONET_BASE_URL = "http://shironet.mako.co.il"
     SHIRONET_AGENT = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
-    SHIRONET_COOKIE = {'rbzid': 'Y2ZYVmtJRnhaRUR4TVdZNjZ6TVpEcUliaTFFa1hWUk51eXBrUldVcXNtMzhqdmp0RkRCZE4wb04zYXh1QlRzWnJiM0dkNGZiTW1tVUtoNENtOVVEb1Q0N2hoZm9HM1AxcjhjajE4UGM4VXZEQ0ZDUy9EZ2c5bjBYcnphcmxFdUlxOCszTXJlWHprc2QzenRGSy84VS9EeUp6d1I5dWNZdmJiQmxsellqcysxaW9JbXpoVVc1K0MzdjFBWXQxWm8raW1oeHVyUGtQTERPYnNrUTh6bjBPUT09QEBAMUBAQC0xNDgxNDgxNDY4MA'}
+    SHIRONET_COOKIE = {'rbzid': 'WjBhcWcwUXh0VlV1WW1rSHYzdHF2MExVVUh3aHV3S25GeVNIbmo4RHZ3Y1A2NWU1eHlPV2VRY2ZTVUMzaUhaRWNKc0RzWjhGbEJIemNOTUJhQjBBSU1VU2g5OFBlZ3hUWVBXSGZwcnhJR2VNdmZub3YvaDNuaTFJeERlam5pcXlZM3JDS2M5blR1d0QycE9PMFE3a2RoUEpDZHExQzdHc2tXZTdkeUFtNnBiZzVXMGVOTFEyK3BERi93cWxGYW5aTmo3Q1p2K3JwM0hVeUQ4WU1XMzVpZz09QEBAMkBAQC0xNDgxNDgxNDY4MA'}
 
     ARTIST_PAGE_PREFIX = "http://shironet.mako.co.il/artist?type=works&lang=1&prfid="
     ARTISTS_URLS = [
@@ -15,5 +15,19 @@ class shironet_config:
         #artist_url("Meir Ariel", ARTIST_PAGE_PREFIX + '605'),
         #artist_url("Hadag Nahash", ARTIST_PAGE_PREFIX + '333'),
         #artist_url("Lior Narkis", ARTIST_PAGE_PREFIX + '594'),
-        artist_url("Mooki", ARTIST_PAGE_PREFIX + '620')
+        #artist_url("Mooki", ARTIST_PAGE_PREFIX + '620'),
+        #artist_url("Peer Tasi", ARTIST_PAGE_PREFIX + '7747'),
+        #artist_url("Eyal Golan", ARTIST_PAGE_PREFIX + '92'),
+        #artist_url("Omer Adam", ARTIST_PAGE_PREFIX + '10755'),
+        #artist_url("Regev Hod", ARTIST_PAGE_PREFIX + '2570')
+        #artist_url("Sarit Hadad", ARTIST_PAGE_PREFIX + '1015')
+        #artist_url("Shlomo Artzi", ARTIST_PAGE_PREFIX + '975')
+        #artist_url("Shalom Hanoch", ARTIST_PAGE_PREFIX + '960')
+        #artist_url("Arik Einstein", ARTIST_PAGE_PREFIX + '166')
+        #artist_url("Rita", ARTIST_PAGE_PREFIX + '905')
+        #artist_url("Miri Mesika", ARTIST_PAGE_PREFIX + '4226')
+        #artist_url("Keren Peles", ARTIST_PAGE_PREFIX + '4314')
+        #artist_url("Assaf Amdursky", ARTIST_PAGE_PREFIX + '150')
+        #artist_url("Avraham Tal", ARTIST_PAGE_PREFIX + '7335')
+        artist_url("Mashina", ARTIST_PAGE_PREFIX + '686')
     ]
